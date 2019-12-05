48435cc0db26429d6384e9b3449dbbe50fff8378
roaslin <roaslin@gmail.com>
2019-12-05 15:04:48 +0100

    fix mock issue, now passes the test for the right reason

--- src/AccountService.js
+++ src/AccountService.js
@@ -3,9 +3,5 @@ import Console from "../src/Console"
 export default class AccountService {
   deposit(amount){} 
   withdraw(amount){} 
-  printStatement(){
-
-    const console = new Console()
-    console.printLn("Date || Amount || Balance")
-  }
+  printStatement(){}
 }
--- test/BankAccountFeature.test.js
+++ test/BankAccountFeature.test.js
@@ -1,19 +1,23 @@
 import AccountService from "../src/AccountService"
-import * as console from "../src/Console"
+import Console from "../src/Console"
+jest.mock("../src/Console")
 
-jest.mock("../src/Console.js")
 
 describe('Print statements', () => {
-  const accountService = new AccountService()
+  it('Print statements', () => {
+    const accountService = new AccountService()
+    
+    accountService.deposit(3000)
+    accountService.deposit(1000)
+    accountService.withdraw(500)
 
-  accountService.deposit(3000)
-  accountService.deposit(1000)
-  accountService.withdraw(500)
+    accountService.printStatement()
 
-  accountService.printStatement()
+    const myConsole = Console.mock.instances[0]
 
-  expect(console.printLn).toHaveBeenCalledWith("Date || Amount || Balance")
-  expect(console.printLn).toHaveBeenCalledWith("01/01/2019 || -500 || 3500")
-  expect(console.printLn).toHaveBeenCalledWith("10/12/2018 || 1000 || 4000")
-  expect(console.printLn).toHaveBeenCalledWith("01/09/2018 || 3000 || 3000")
+    expect(myConsole.printLn).toHaveBeenCalledWith("Date || Amount || Balance")
+    expect(myConsole.printLn).toHaveBeenCalledWith("01/01/2019 || -500 || 3500")
+    expect(myConsole.printLn).toHaveBeenCalledWith("10/12/2018 || 1000 || 4000")
+    expect(myConsole.printLn).toHaveBeenCalledWith("01/09/2018 || 3000 || 3000")
+  })
 })
